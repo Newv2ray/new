ab3f80eb039add0a19e6a5c0ebf692cd1a290cf7
Newv2ray <zahrajan920@gmail.com>
2025-08-17 16:51:38 +0330

    Update update.py

--- update.py
+++ update.py
@@ -1,16 +1,17 @@
 import random
 import requests
+import time
 
 # دانلود ساب اصلی
 url = "https://raw.githubusercontent.com/Newv2ray/new/refs/heads/main/iSegaro.txt"
 r = requests.get(url)
 configs = r.text
 
-# ساخت متادیتا رندوم
-upload = random.randint(0, 200) * 1024*1024*1024
-download = random.randint(0, 500) * 1024*1024*1024
+# ساخت حجم رندوم با بایت
+upload = random.randint(0, 200) * 1024 * 1024 * 1024
+download = random.randint(0, 500) * 1024 * 1024 * 1024
 total = 1099511627776
-expire = 2546249531
+expire = int(time.time()) + 30*24*3600  # ۳۰ روز بعد
 
 meta = f"""//profile-update-interval: 1
 //subscription-userinfo: upload={upload}; download={download}; total={total}; expire={expire}
@@ -20,7 +21,7 @@ meta = f"""//profile-update-interval: 1
 full_config = meta + "\n" + configs
 
 # ذخیره در فایل
-with open("sub_ready.txt", "w") as f:
+with open("iSegaro.txt", "w") as f:
     f.write(full_config)
 
-print("ساب آماده شد: sub_ready.txt")
+print("ساب داینامیک آماده شد: iSegaro.txt")
