21b2507739ef95355c854029d4338c62a37ad4fd
Newv2ray <zahrajan920@gmail.com>
2025-08-17 17:07:15 +0330

    Update update.py

--- update.py
+++ update.py
@@ -1,12 +1,11 @@
 import random
+import requests
 import time
 
-# مسیر فایل اصلی
-file_path = "iSegaro.txt"
-
-# خواندن محتوای اصلی
-with open(file_path, "r") as f:
-    configs = f.read()
+# دانلود ساب اصلی
+url = "https://wanmeiwl3.xyz/gywl/945ef01f5d91f355cb3da871436232dc"
+r = requests.get(url)
+configs = r.text
 
 # ساخت حجم رندوم با بایت
 upload = random.randint(0, 200) * 1024 * 1024 * 1024
@@ -14,16 +13,15 @@ download = random.randint(0, 500) * 1024 * 1024 * 1024
 total = 1099511627776
 expire = int(time.time()) + 30*24*3600  # ۳۰ روز بعد
 
-# خط متادیتا
 meta = f"""//profile-update-interval: 1
 //subscription-userinfo: upload={upload}; download={download}; total={total}; expire={expire}
 """
 
-# اضافه کردن متادیتا به ابتدای فایل
+# فایل نهایی با متادیتا
 full_config = meta + "\n" + configs
 
-# ذخیره مجدد همان فایل
-with open(file_path, "w") as f:
+# ذخیره در فایل
+with open("sub_dynamic.txt", "w") as f:
     f.write(full_config)
 
-print("ساب اصلی با متادیتا داینامیک آماده شد!")
+print("ساب داینامیک آماده شد: sub_dynamic.txt")
